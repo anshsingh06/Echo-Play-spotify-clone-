5c158f90e0808c1b7dbbfd5916214461169494ae
anshsingh06 <anshsingh01227@gmail.com>
2025-07-31 00:39:39 +0530

    userRouter defined

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+// import { protectRoute } from "../middleware/auth.middleware.js";
+// import { getAllUsers, getMessages } from "../controller/user.controller.js";
+const router = Router();
+
+// router.get("/", protectRoute, getAllUsers);
+// router.get("/messages/:userId", protectRoute, getMessages);
+
+router.get("/", (req, res) => {
+  res.send("User route is working");
+});
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import adminRoutes from "./routes/adminRoute.js";
+import authRoutes from "./routes/authRoute.js";
+import songRoutes from "./routes/songRoute.js";
+import albumRoutes from "./routes/albumRoute.js";
+import statRoutes from "./routes/statRoute.js";
+
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use("/api/users", userRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
