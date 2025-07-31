cd977e30557135677b63be172185660146e3f45d
anshsingh06 <anshsingh01227@gmail.com>
2025-07-31 11:26:43 +0530

    .env removed from git

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
-import adminRoutes from "./routes/adminRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-import songRoutes from "./routes/songRoutes.js";
-import albumRoutes from "./routes/albumRoutes.js";
-import statRoutes from "./routes/statRoutes.js";
+// import adminRoutes from "./routes/adminRoutes.js";
+// import authRoutes from "./routes/authRoutes.js";
+// import songRoutes from "./routes/songRoutes.js";
+// import albumRoutes from "./routes/albumRoutes.js";
+// import statRoutes from "./routes/statRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 
@@ -17,11 +17,11 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use("/api/users", userRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/songs", songRoutes);
-app.use("/api/albums", albumRoutes);
-app.use("/api/stats", statRoutes);
+// app.use("/api/admin", adminRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/songs", songRoutes);
+// app.use("/api/albums", albumRoutes);
+// app.use("/api/stats", statRoutes);
 
 
 
