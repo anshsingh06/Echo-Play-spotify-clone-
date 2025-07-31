83abaea3ca66a352a62f3864b9e0453a14ddb720
anshsingh06 <anshsingh01227@gmail.com>
2025-07-31 11:59:33 +0530

    sign and login auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 // import adminRoutes from "./routes/adminRoutes.js";
-// import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 // import songRoutes from "./routes/songRoutes.js";
 // import albumRoutes from "./routes/albumRoutes.js";
 // import statRoutes from "./routes/statRoutes.js";
@@ -18,10 +18,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/users", userRoutes);
 // app.use("/api/admin", adminRoutes);
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 // app.use("/api/songs", songRoutes);
 // app.use("/api/albums", albumRoutes);
 // app.use("/api/stats", statRoutes);
+
+app.use(express.json());
 
 
 
