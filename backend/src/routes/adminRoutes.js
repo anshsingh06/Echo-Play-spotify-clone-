57d93a3f095685781599a64c1cba8b227f292f60
anshsingh06 <anshsingh01227@gmail.com>
2025-08-01 09:35:04 +0530

    export error fixed

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
-import { checkAdmin, createAlbum, createSong, deleteAlbum, deleteSong } from "../controller/admin.controller.js";
-import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+// import { checkAdmin, createAlbum, createSong, deleteAlbum, deleteSong } from "../controller/adminController.js";
+import { protectRoute, requireAdmin } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.use(protectRoute, requireAdmin);
 
-router.get("/check", checkAdmin);
+// router.get("/check", checkAdmin);
 
-router.post("/songs", createSong);
-router.delete("/songs/:id", deleteSong);
+// router.post("/songs", createSong);
+// router.delete("/songs/:id", deleteSong);
 
-router.post("/albums", createAlbum);
-router.delete("/albums/:id", deleteAlbum);
+// router.post("/albums", createAlbum);
+// router.delete("/albums/:id", deleteAlbum);
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import {clerkMiddleware} from "@clerk/express";
 import userRoutes from "./routes/userRoutes.js";
-// import adminRoutes from "./routes/adminRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 // import songRoutes from "./routes/songRoutes.js";
 // import albumRoutes from "./routes/albumRoutes.js";
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use("/api/users", userRoutes);
-// app.use("/api/admin", adminRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 // app.use("/api/songs", songRoutes);
 // app.use("/api/albums", albumRoutes);
