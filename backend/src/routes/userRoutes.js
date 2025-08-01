6f7c6c9578ca30ce28cd43abdf05c9145d8755b0
anshsingh06 <anshsingh01227@gmail.com>
2025-08-01 09:04:13 +0530

    user display

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-// import { protectRoute } from "../middleware/auth.middleware.js";
-// import { getAllUsers, getMessages } from "../controller/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers, getMessages } from "../controller/user.controller.js";
 const router = Router();
 
-// router.get("/", protectRoute, getAllUsers);
-// router.get("/messages/:userId", protectRoute, getMessages);
+router.get("/", protectRoute, getAllUsers);
+router.get("/messages/:userId", protectRoute, getMessages);
 
 router.get("/", (req, res) => {
   res.send("User route is working");
