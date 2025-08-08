c3dc489725b19b422fedbc7e5088243c875edb4a
anshsingh06 <anshsingh01227@gmail.com>
2025-08-08 23:13:53 +0530

    new changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 // import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/clerk-react"
 import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import AuthCallbackPage from './pages/authCallback/authCallback' 
 function App() {
  
   return (
     <>
       <Routes>
-        <Route path="/" element={HomePage />} />
-        <Route path="/auth-callback" element={AuthCallbackPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
       </Routes>
     </>
   )
---
+++ frontend/src/pages/authCallback/authCallback.tsx
@@ -0,0 +1,9 @@
+const AuthCallbackPage =() =>{
+    return (
+        <div>
+            <h1>Authentication Callback</h1>
+            <p>This page handles the authentication callback from the OAuth provider.</p>
+        </div>  
+    )
+}
+export default AuthCallbackPage
